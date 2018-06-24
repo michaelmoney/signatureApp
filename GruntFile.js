f304957733d09a5489a7e150e7a57368747a3441
Michal Pieniazek <mpieniazek@me.com>
2018-06-24 16:25:44 +0200

    Remove comments and add image for README

--- GruntFile.js
+++ GruntFile.js
@@ -1,71 +1,68 @@
 module.exports = function(grunt) {
-
-
-
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        //uglify: {
-        //    build: {
-        //        src: ["js/*.js", "js/lib/*.js"],
-        //        dest: 'build/js/all.min.js'
-        //    }
-        //},
-        //cssmin: {
-        //    target: {
-        //        files: [{
-        //            expand: true,
-        //            cwd: 'css',
-        //            src: ['styles.css', '!*.min.css'],
-        //            dest: 'build/css/',
-        //            ext: '.min.css'
-        //        }]
-        //    }
-        //},
-        //concat_css: {
-        //    options: {
-        //        // Task-specific options go here.
-        //    },
-        //    all: {
-        //        src: ["css/*.css"],
-        //        dest: "build/css/all.css"
-        //    }
-        //},
-        //pngmin: {
-        //    compile: {
-        //        options: {
-        //            concurrency: 8,             // specify how many exucutables get spawned in parallel
-        //            colors: 128,                // reduce colors to 128
-        //            ext: '.png',                // use .png as extension for the optimized files
-        //            quality: '55-70',           // output quality should be between 65 and 80 like jpeg quality
-        //            speed: 10,                  // pngquant should be as fast as possible
-        //            iebug: true                 // optimize image for use in Internet Explorer 6
-        //        },
-        //        files: [
-        //            {
-        //                src: 'img/*.png',
-        //                dest: 'build/img'
-        //            }
-        //        ]
-        //    }
-        //},
-        //imagemin: {
-        //
-        //    dynamic: {
-        //        options: {
-        //            optimizationLevel: 7,
-        //            svgoPlugins: [{ removeViewBox: false }],
-        //            progressive: true,
-        //            use: [mozjpeg()]
-        //        },
-        //        files: [{
-        //            expand: true,                  // Enable dynamic expansion
-        //            cwd: 'img/',                   // Src matches are relative to this path
-        //            src: ['*/*.jpg', '*.jpg'],   // Actual patterns to match
-        //            dest: 'build/img/'                  // Destination path prefix
-        //        }]
-        //    }
-        //},
+        uglify: {
+           build: {
+               src: ["js/*.js", "js/lib/*.js"],
+               dest: 'build/js/all.min.js'
+           }
+        },
+        cssmin: {
+           target: {
+               files: [{
+                   expand: true,
+                   cwd: 'css',
+                   src: ['styles.css', '!*.min.css'],
+                   dest: 'build/css/',
+                   ext: '.min.css'
+               }]
+           }
+        },
+        concat_css: {
+           options: {
+               // Task-specific options go here.
+           },
+           all: {
+               src: ["css/*.css"],
+               dest: "build/css/all.css"
+           }
+        },
+        pngmin: {
+           compile: {
+               options: {
+                   concurrency: 8,             // specify how many exucutables get spawned in parallel
+                   colors: 128,                // reduce colors to 128
+                   ext: '.png',                // use .png as extension for the optimized files
+                   quality: '55-70',           // output quality should be between 65 and 80 like jpeg quality
+                   speed: 10,                  // pngquant should be as fast as possible
+                   iebug: true                 // optimize image for use in Internet Explorer 6
+               },
+               files: [
+                   {
+                       src: 'img/*.png',
+                       dest: 'build/img'
+                   }
+               ]
+           }
+        },
+        imagemin: {
+
+           dynamic: {
+               options: {
+                   optimizationLevel: 7,
+                   svgoPlugins: [{ removeViewBox: false }],
+                   progressive: true,
+                   use: [mozjpeg()]
+               },
+               files: [{
+                   expand: true,                  // Enable dynamic expansion
+                   cwd: 'img/',                   // Src matches are relative to this path
+                   src: ['*/*.jpg', '*.jpg'],   // Actual patterns to match
+                   dest: 'build/img/'                  // Destination path prefix
+               }]
+           }
+        },
         jslint: { // configure the task
             client: {
                 src: [
@@ -82,29 +79,29 @@ module.exports = function(grunt) {
                     errorsOnly: true
                 }
             }
+        },
+        copy: {
+           main: {
+               files: [
+                   // includes files within path
+                   // includes files within path and its sub-directories
+                   {expand: true, src: ['fonts/**'], dest: 'build/'},
+                   {expand: true, src: ['portfolio/**'], dest: 'build/'},
+                   {expand: true, src: ['send/**'], dest: 'build/'},
+                   {expand: true, src: ['index.html'], dest: 'build/'},
+                   {expand: true, src: ['img/icons/**'], dest: 'build/'},
+                   {expand: true, src: ['files/*'], dest: 'build/'},
+                   {expand: true, src: ['css/*.css'], dest: 'build/'},
+               ]
+           }
+        },
+        processhtml: {
+           build: {
+               files: {
+                   'build/index.html': ['index.html']
+               }
+           }
         }
-        //copy: {
-        //    main: {
-        //        files: [
-        //            // includes files within path
-        //            // includes files within path and its sub-directories
-        //            {expand: true, src: ['fonts/**'], dest: 'build/'},
-        //            {expand: true, src: ['portfolio/**'], dest: 'build/'},
-        //            {expand: true, src: ['send/**'], dest: 'build/'},
-        //            {expand: true, src: ['index.html'], dest: 'build/'},
-        //            {expand: true, src: ['img/icons/**'], dest: 'build/'},
-        //            {expand: true, src: ['files/*'], dest: 'build/'},
-        //            {expand: true, src: ['css/*.css'], dest: 'build/'},
-        //        ]
-        //    }
-        //},
-        //processhtml: {
-        //    build: {
-        //        files: {
-        //            'build/index.html': ['index.html']
-        //        }
-        //    }
-        //}
     });
 
     // Load the plugins
